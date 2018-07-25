14b79192771269c14d69b26d958408d4a16a0771
Matthew Contract <mcontract27@gmail.com>
2018-07-25 12:10:19 -0400

    added customers component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
-import { ModuleWithProviders } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 // Project Components
-import { HomeComponent } from './home/home.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
-
+import { HomeComponent } from "./home/home.component";
+import { EditCustomerComponent } from "./edit-customer/edit-customer.component";
+import { CustomersComponent } from "./customers/customers.component";
 
 const appRoutes: Routes = [
-    // Getting Started
-    { path: '', component: HomeComponent },
+  // Getting Started
+  { path: "", component: HomeComponent },
 
-    // Customer
-    { path: 'edit-customer/:id', component: EditCustomerComponent },
+  // Edit Customer
+  { path: "edit-customer/:id", component: EditCustomerComponent },
 
-    // otherwise redirect to home
-    // { path: '**', redirectTo: '' },
-   ];
+  // Customer List
+  { path: "customers", component: CustomersComponent }
+
+  // otherwise redirect to home
+  // { path: '**', redirectTo: '' },
+];
 
 export const AppRoutes: ModuleWithProviders = RouterModule.forRoot(appRoutes);
